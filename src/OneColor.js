1813619cf0befa3ec9f4bcd7cb8ea44a0e23cff8
Asha <asha17@brown.edu>
2020-12-30 15:39:57 -0800

    React Color Generator!
    Focus on JSX and React, using provided CSS. Added a fun icon and a personal color. I coded in automatic copying to clipboard.

---
+++ src/OneColor.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import rgbToHex from "./utils";
+
+const OneColor = ({rgb, weight, index, hexColor}) => {
+    const [alert, setAlert] = useState(false);
+    const backg = rgb.join(",");
+    const hex = rgbToHex(...rgb)
+    const hexValue = `#${hexColor}`
+    useEffect(() => {
+        const timeout = setTimeout(() =>{
+            setAlert(false)
+        }, 3000)
+        return () => clearTimeout(timeout)
+    }, [alert])
+    return <article
+        className={`color ${index > 10 && "color-light"}`}
+        style={{backgroundColor: `rgb(${backg})`}}
+        onClick={() => {
+            setAlert(true);
+            navigator.clipboard.writeText(hexValue);
+        }}>
+     <p className="percent-value">
+         {weight}%
+     </p>
+     <p className="color-value">{hexValue}</p>
+     {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+};
+
+export default OneColor;
